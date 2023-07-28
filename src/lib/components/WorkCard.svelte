3194d1e41547de46ebc661cdd0f50ccc10d68556
fbottarelli <federico.bottarelli@gmail.com>
2023-07-28 02:05:04 +0200

    mongodb

---
+++ src/lib/components/WorkCard.svelte
@@ -0,0 +1,11 @@
+<script>
+    export let work;
+  </script>
+  
+  <div class="card">
+    <img src={work.image} alt={work.title} />
+    <h2>{work.title}</h2>
+    <p>{work.description}</p>
+    <a href={work.link}>View Work</a>
+  </div>
+  
