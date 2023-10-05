2473bd017b913069acd59e66863f57b135860bf6
fbottarelli <federico.bottarelli@gmail.com>
2023-10-05 23:33:47 +0200

    useless fixes

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,16 +8,11 @@ import { motion } from "framer-motion";
 const Nav = () => {
     return (
         <>
-            <motion.div className="fixed right-1/2 h-[3rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.1rem] md:translate-x-[-100px] md:top-12 md:h-[12rem] md:w-[4rem] md:rounded-lg dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
-                       initial={{ y: -100, x: "+50%", opacity: 0 }}
-                       animate={{ y: 0, x: "+50%", opacity: 1 }}>
-            </motion.div>
-
             {/* <div className="sticky"> */}
-{/*            <motion.div className="fixed top-0 right-1/2 h-[3rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] md:top-12 md:h-[10rem] md:w-[30rem] md:rounded-lg dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
+            {/* <motion.div className="fixed top-0 right-1/2 h-[3rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-50 shadow-lg shadow-black/[0.03] backdrop-blur-[0.1rem] lg:top-12 lg:h-[10rem] lg:w-[30rem] lg:rounded-lg dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
                         initial={{ y: -100, x: "+50%", opacity: 0 }}
                         animate={{ y: 0, x: "+50%", opacity: 1 }}>
-            </motion.div>*/}
+            </motion.div> */}
             <nav className="lg:fixed lg:top-8 lg:left-20 fixed top-8 flex flex-row lg:flex-col mb-10 lg:mb-0">
                 {/*<div className="flex flex-row md:flex-col">*/}
                     <Link href="/" className="mr-12 lg:mb-4 font-extrabold text-xl">Home</Link>
