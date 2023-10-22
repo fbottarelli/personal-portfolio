54cd973c11d2f1cb597b4ce7fe04dfcd208b58cd
fbottarelli <federico.bottarelli@gmail.com>
2023-10-22 18:31:32 +0200

    bug fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Footer from "@/components/footer"
 import Header from "@/components/header"
-import UnderHeader from "@/components/underHeader"
 
 import ActiveSectionContextProvider from "@/context/active-section-context";
 
