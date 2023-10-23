f1f295207d6d294254f548d9ba70ad5e1499e120
fbottarelli <federico.bottarelli@gmail.com>
2023-10-23 20:41:42 +0200

    CV and contact button

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -3,12 +3,12 @@
 // import Image from "next/image";
 import React from "react";
 // import { motion } from "framer-motion";
-// import Link from "next/link";
-import {  BsLinkedin } from "react-icons/bs";
-// import { HiDownload } from "react-icons/hi";
+import Link from "next/link";
+import { HiDownload } from "react-icons/hi";
 import { FaGithubSquare } from "react-icons/fa";
 import { useSectionInView } from "@/lib/hooks";
 import { motion } from "framer-motion";
+import { BsArrowRight, BsLinkedin } from "react-icons/bs";
 // import { useActiveSectionContext } from "@/context/active-section-context";
 
 export default function Intro() {
@@ -21,6 +21,7 @@ export default function Intro() {
       id="home"
       className="mb-28 max-w-[50rem] sm:mb-0 scroll-mt-[100rem] font-sans text-left"
     >
+      {/* Title */}
       <motion.div
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
@@ -32,6 +33,21 @@ export default function Intro() {
             <h1 className="bg-gradient-to-r from-purple-500 via-pink-500 to-blue-500 bg-clip-text text-transparent mt-8">
                 Federico <br/> 
                 Bottarelli
+            </h1>
+        </div>
+      </motion.div>
+{/* subtitle */}
+      <motion.div
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.1,
+        }}
+      >
+        <div className="text-2xl mb-8">
+            <h1 className="bg-gradient-to-r from-purple-500 via-pink-500 to-blue-500 bg-clip-text text-transparent mt-8">
+                Data Science, DataOps, AI Specialist <br/> 
+                
             </h1>
         </div>
       </motion.div>
@@ -57,7 +73,7 @@ export default function Intro() {
             delay: 0.4,
           }}
       >
-      <p className="mb-6 text-xl">
+      <p className="mb-6 text-2xl">
         Currently, I&apos;m pursuing a <span className="font-bold"> Data Science master&apos;s degree</span> at
          Padua University. Alongside my studies, I&apos;m working as a
         <span className="font-bold"> Junior DataOps Engineer</span> for an Italian company.
@@ -71,41 +87,43 @@ export default function Intro() {
             delay: 0.5,
           }}
       >
-        <p className='text-xl my-6'>
-          From classical <strong>statistical analysis</strong> to more technology-based approaches like using <strong>AI</strong>, always with a statistical and mathematical approach in mind. I&apos;m fast and experienced with computers and modern technologies. I have a good base of <strong>coding</strong> skills, particularly in Python and R, but I&apos;m always open to learning new tools and technology.  🍋
+        <p className='text-2xl my-6'>
+          From classical <strong>statistical analysis</strong> to more technology-based approaches like using <strong>AI</strong>, always with a statistical and mathematical approach in mind. I&apos;m fast and experienced with computers and modern technologies. I have a good base of <strong>coding</strong> skills, particularly in Python and R, but I&apos;m always open to learning new tools and technology.
         </p>
         </motion.div>
 
 
 
-      <motion.div
-        className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium mt-10 mb-10"
-        initial={{ opacity: 0, y: 100 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{
-          delay: 0.3,
-        }}
-      >
-        {/*<Link*/}
-        {/*  href="#contact"*/}
-        {/*  className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"*/}
-        {/*  onClick={() => {*/}
-        {/*    setActiveSection("Contact");*/}
-        {/*    setTimeOfLastClick(Date.now());*/}
-        {/*  }}*/}
-        {/*>*/}
-        {/*  Contact me here{" "}*/}
-        {/*  <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />*/}
-        {/*</Link>*/}
+        <motion.div
+          className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium mt-10 mb-10"
+          initial={{ opacity: 0, y: 100 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{
+            delay: 0.3,
+          }}
+        >
+          
+        <Link
+          href="#contact"
+          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+          // onClick={() => {
+          //   setActiveSection("Contact");
+          //   setTimeOfLastClick(Date.now());
+          // }}
+        >
+          Contact me {" "}
+          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+        </Link>
 
-        {/*<a*/}
-        {/*  className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"*/}
-        {/*  href="/CV.pdf"*/}
-        {/*  download*/}
-        {/*>*/}
-        {/*  Download CV{" "}*/}
-        {/*  <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />*/}
-        {/*</a>*/}
+        <a
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
+          href="/FedericoBottarelli_CV.pdf"
+          download
+        >
+          Download CV{" "}
+          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+        </a>
+
 
         <a
           className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -29,25 +29,33 @@ export default function Skills() {
       ref={ref}
       className="mb-12 max-w-2xl scroll-mt-28 text-center sm:mb-24"
     >
-
+      <motion.div
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.4,
+        }}
+      >
         <SectionHeading>My Skills</SectionHeading>
-      <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
-        {skillsData.map((skill, index) => (
-          <motion.li
-            className="bg-white borderBlack rounded-xl px-5 py-3 dark:bg-white/10 dark:text-white/80"
-            key={index}
-            variants={fadeInAnimationVariants}
-            initial="initial"
-            whileInView="animate"
-            viewport={{
-              once: true,
-            }}
-            custom={index}
-          >
-            {skill}
-          </motion.li>
+        </motion.div>
+        <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
+          {skillsData.map((skill, index) => (
+            <motion.li
+              className="bg-white borderBlack rounded-xl px-5 py-3 dark:bg-white/10 dark:text-white/80"
+              key={index}
+              variants={fadeInAnimationVariants}
+              initial="initial"
+              whileInView="animate"
+              viewport={{
+                once: true,
+              }}
+              custom={index}
+            >
+              {skill}
+            </motion.li>
         ))}
       </ul>
+
     </section>
   );
 }
