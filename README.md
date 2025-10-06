bf303e0f4edab8c544efba830f02f02b55504250
fbottarelli <f.bottarelli@miriade.it>
2025-10-06 14:35:20 +0200

    remove all

--- README.md
+++
@@ -1,2 +0,0 @@
-# personal-portfolio
-Repo for my personal website, made for personal training with Next-JS and Svelte-kit (see branches)
--- src/app/posts/[postId]/page.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-
-export default function Page({ params }: { params: { postId: string } }) {
-  return <div>My Post: {params.postId}</div>
-}
-
